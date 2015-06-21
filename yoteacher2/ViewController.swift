f02e8da28f4535c4237f94b76c473b27d1b631b1
zhuolinho <zhuolinho@hotmail.com>
2015-06-21 16:15:09 +0800

    first commit

---
+++ yoteacher2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  yoteacher2
+//
+//  Created by HoJolin on 15/6/21.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
